00e26835669c2dab77b52fd2b0c29722d1c9b821
Cullen <cullen@cullenwatson.com>
2023-07-09 18:48:48 -0500

    docs: remove invalid token route

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter, Depends
 from .jobs import router as jobs_router
-from api.auth.token import router as token_router
 from api.auth.auth_utils import get_active_current_user
 
 router = APIRouter(prefix="/v1", dependencies=[Depends(get_active_current_user)])
 router.include_router(jobs_router)
-router.include_router(token_router)
