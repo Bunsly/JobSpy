2a96fdc8a557bdf14dedc2f28ac55fa53099f827
Cullen Watson <cullen@cullenwatson.com>
2023-07-11 08:51:19 -0500

    fix(jobs): add 1 to loop counter

--- api/core/scrapers/ziprecruiter/__init__.py
+++ api/core/scrapers/ziprecruiter/__init__.py
@@ -114,7 +114,10 @@ class ZipRecruiterScraper(Scraper):
                     job_url=job_url,
                 )
                 job_list.append(job_post)
-                if len(job_list) >= scraper_input.results_wanted:
+                if (
+                    len(job_list) >= scraper_input.results_wanted
+                    or processed_jobs >= job_count
+                ):
                     break
 
             if (
