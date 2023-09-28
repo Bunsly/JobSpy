c676050dc0fc48fc4f0bec936d09634a57117059
Cullen Watson <cullen@cullenwatson.com>
2023-09-28 18:33:02 -0500

    [fix] util func

--- src/jobspy/utils.py
+++
@@ -1,9 +0,0 @@
-import re
-from typing import Optional
-
-
-def extract_emails_from_text(text: str) -> Optional[list[str]]:
-    if not text:
-        return None
-    email_regex = re.compile(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}")
-    return email_regex.findall(text)
