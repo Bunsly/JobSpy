90132ca1990b8d2c6f77e9c835e19b65ffb6b78e
Yariv Menachem <yariv220895@gmail.com>
2025-01-03 18:48:54 +0200

    fixed env

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,25 +1,16 @@
 import os
 
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
-if not os.getenv("ENV"):
+if not os.environ.get("ENV"):
     raise ValueError("Invalid environment. Set the 'ENV' variable (e.g., export ENV=dev).")
-
-dotenv_file = os.path.join(os.path.dirname(__file__), ".env." + os.environ.get("ENV"))
-
-if not os.path.exists(dotenv_file):
-    raise FileNotFoundError(f"Environment file not found: {dotenv_file}")
 
 
 class Settings(BaseSettings):
-    environment: str
-    telegram_api_token: str
-    mongo_uri: str
-    mongo_db_name: str
-    print(f"Loading environment from: {dotenv_file}")
-    model_config = SettingsConfigDict(
-        env_file=dotenv_file
-    )
+    telegram_api_token: str = Field(alias="telegram_api_token")
+    mongo_uri: str = Field(alias="mongo_uri")
+    mongo_db_name: str = Field(alias="mongo_db_name")
 
 
 settings = Settings()
